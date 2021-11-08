474520093a77e501b60c7da3a79b6052b4858d42
Somesh <someshiet@gmail.com>
2021-11-08 11:27:30 +0530

    Initial commit

---
+++ src/main/java/com/vermaji/hangup/HangupApplication.java
@@ -0,0 +1,13 @@
+package com.vermaji.hangup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HangupApplication.class, args);
+	}
+
+}
